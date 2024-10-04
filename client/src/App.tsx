65eff426161f1ab76e25f30a6c9e88da02864a62
arkia1 <arkia.ebrahimi84@gmail.com>
2024-10-04 20:37:28 +0300

    file system fixes

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className=" flex justify-center items-center">
+      <div className="bg-slate-600 w-24 h-24"></div>
+    </div>
+  );
+};
+
+export default App;
