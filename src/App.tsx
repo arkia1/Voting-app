3c95ca8774a232ff159c31087006e55217c5cec3
arkia1 <arkia.ebrahimi84@gmail.com>
2024-10-04 20:38:47 +0300

    delete the old files

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className=" flex justify-center items-center">
-      <div className="bg-slate-600 w-24 h-24"></div>
-    </div>
-  );
-};
-
-export default App;
